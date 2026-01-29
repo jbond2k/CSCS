33864909ba63d01a409a1a7369cc22e7faea7944
jbond2k <jbond@eagles.bridgewater.edu>
2026-01-28 22:12:38 -0500

    Commit Assignment 3 Source Code

--- App2/src/App.vue
+++ App2/src/App.vue
@@ -1,11 +1,78 @@
-<script setup></script>
+<script setup>
+import { reactive, onMounted } from 'vue'
+import GridLayout from './components/GridLayout.vue'
+import imga from './assets/imga.jpg'
+import imgb from './assets/imgb.jpg'
+import imgc from './assets/imgc.jpg'
+import imgd from './assets/imgd.jpg'
+import imge from './assets/imge.jpg'
+import imgf from './assets/imgf.jpg'
+import imgg from './assets/imgg.jpg'
+
+const slots = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+
+const correctImages = {
+  a: imga,
+  b: imgb,
+  c: imgc,
+  d: imgd,
+  e: imge,
+  f: imgf,
+  g: imgg
+}
+
+const images = reactive({ correctImages })
+
+function scramble() {
+  const scrambledImages = Object.values(correctImages).sort(() => Math.random() - 0.5)
+
+  slots.forEach((slot, i) => {
+    images[slot] = scrambledImages[i]
+  })
+}
+
+onMounted(() => {
+  scramble()
+})
+
+function identify() {
+  for (const image in correctImages) {
+    images[image] = correctImages[image]
+  }
+}
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div>
+    <GridLayout>
+      <template v-for="slot in slots" :key="slot" v-slot:[slot]>
+        <img :src="images[slot]" />
+      </template>
+    </GridLayout>
+
+    <div class="buttons">
+      <button @click="scramble">Scramble</button>
+      <button @click="identify">Identify</button>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.buttons {
+  margin-top: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+button {
+  margin-inline: 20px;
+  padding: 3px 6px;
+  font-size: 20px;
+}
+
+img {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ App2/src/components/GridLayout.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <div class="grid">
+            <div class="a"><slot name="a" /></div>
+            <div class="b"><slot name="b" /></div>
+            <div class="c"><slot name="c" /></div>
+            <div class="d"><slot name="d" /></div>
+            <div class="e"><slot name="e" /></div>
+            <div class="f"><slot name="f" /></div>
+            <div class="g"><slot name="g" /></div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.grid {
+    display: grid;
+    width: 100vw;
+    height: 90vh;
+    grid-template-columns: repeat(7, 1fr);
+    grid-template-rows: repeat(7, 1fr);
+    grid-template-areas:
+    "a a a b b c c"
+    "a a a b b c c"
+    "d d d b b c c"
+    "d d d b b f f"
+    "d d d e e f f"
+    "g g g g g g g"
+    "g g g g g g g"
+}
+
+.a { grid-area: a; } /* 3×2 */
+.b { grid-area: b; } /* 2×4 */
+.c { grid-area: c; } /* 2×3 */
+.d { grid-area: d; } /* 3×3 */
+.e { grid-area: e; } /* 2×1 */
+.f { grid-area: f; } /* 2×2 */
+.g { grid-area: g; } /* 7×2 */
+
+div {
+    overflow: hidden;
+    width: 100%;
+    height: 100%;
+}
+</style>
